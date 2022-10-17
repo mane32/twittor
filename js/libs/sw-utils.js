bb13db98af8119fb8a659acc78dec5adedc590c2
Vanne Vega <manelp3214@gmail.com>
2022-10-17 13:03:11 -0500

    Cache con network

---
+++ js/libs/sw-utils.js
@@ -0,0 +1,15 @@
+// Guardad en el cache dinamico
+
+function actualizarCacheDinamico(dynamicCache, req, res ) {
+     if( res.ok) {
+         return caches.open( dynamicCache).then(cache => {
+              cache.put(req, res.clone());
+
+              return res.clone();
+        });
+
+     } else {
+        return res;
+     }
+
+}
--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
+// imports 
+
+importScripts('js/sw-utils.js');
 
 
-
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
@@ -16,7 +18,9 @@ const APP_SHELL = [
     'img/avatars/ironman.jpg',
     'img/avatars/spiderman.jpg',
     'img/avatars/wolverine.jpg',
-    'js/app.js'
+    'js/app.js',
+    'js/sw-utils.js'
+
 ];
 
 const APP_SHELL_INMUMTABLE = [
@@ -44,5 +48,22 @@ self.addEventListener('activate', e => {
             return caches.delete(key);
           }
      });
+     e.waitUntil(respuesta);
 });
 
+
+self.addEventListener('fetch', e => {
+      const respuesta = caches.match(e.request).then(res => {
+        if(res) {
+            return res;
+        } else {
+
+              return fetch(e.request).then(newRes => {
+                return actualizarCacheDinamico(DYNAMIC_CACHE, e.request, newRes);
+
+              });
+        }
+
+      });
+      e.respondWith();
+});

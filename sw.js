baaef115f251e830854ef0b6606585e102cdce0b
Vanne Vega <manelp3214@gmail.com>
2022-10-18 10:52:55 -0500

    Modificamos para iphone

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@
 importScripts('js/sw-utils.js');
 
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -47,6 +47,9 @@ self.addEventListener('activate', e => {
           keys.forEach(key => {
             if( key !== STATIC_CACHE && key.includes('static')) {
               return caches.delete(key);
+          }
+          if( key !== DYNAMIC_CACHE && key.includes('dynamic')) {
+            return caches.delete(key);
           }
         });
      });
